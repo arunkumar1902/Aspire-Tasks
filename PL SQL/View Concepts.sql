a1eb06d1c8311e7f0321178654dc9fda8e9af7f8
Arun Kumar <arun.kumar@aspiresys.com>
2024-11-22 08:52:39 +0530

    update

--- PL SQL/View Concepts.sql
+++ PL SQL/View Concepts.sql
@@ -5,3 +5,4 @@ select * from employee;
 -- view concept to display details of employee from chennai and TN team
 create view tn_emp as select * from employee where job_location = 'chennai' and team = 'TN';
 select * from tn_emp;
+

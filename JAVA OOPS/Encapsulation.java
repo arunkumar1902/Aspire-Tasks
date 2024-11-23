2deef941561b84f6c73af7dd3d2cd33f044dc9f6
Arun Kumar <arun.kumar@aspiresys.com>
2024-11-23 11:51:44 +0530

    OOPS

---
+++ JAVA OOPS/Encapsulation.java
@@ -0,0 +1,57 @@
+package com.aspiresys;
+
+class Employee{
+	private String employee_name;
+	private int employee_id;
+	private int employee_age;
+	
+	// constructor
+	public Employee(String name, int id, int age) {
+		this.employee_name = name;
+		this.employee_id = id;
+		this.employee_age = age;
+	}
+	
+	
+	public void display() {
+		System.out.println("Name : "+ employee_name +"\n"+ "Id : "+ employee_id +"\n" +"Age : "+ employee_age);
+	}
+	
+	//Setter
+	public void setEmployeeDetails(String name, int id,int age) {
+	
+		this.employee_name = name;
+		this.employee_id = id;
+		this.employee_age = age;
+	
+	}
+	
+	//getter
+	public String getName(){
+		return employee_name;
+	}
+	public int getId(){
+		return employee_id;
+	}
+	public int getAge(){
+		return employee_age;
+	}
+	
+}
+
+class Encapsulation {
+
+	public static void main(String[] args) {
+		Employee employee_obj = new Employee("Arun",101,22);
+		
+		employee_obj.display();//display the values passed
+		
+		employee_obj.setEmployeeDetails("Bhadri",102,23);//update using setter
+		
+		
+		System.out.println("Name : " + employee_obj.getName());
+		System.out.println("Id : " + employee_obj.getId());
+		System.out.println("Age : " + employee_obj.getAge());
+	}
+
+}
